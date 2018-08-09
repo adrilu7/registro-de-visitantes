c820ccb5091e2e26daccbf2d29a9021d25667331
soberanesmajo <soberanesmajo@gmail.com>
2018-08-09 10:07:56 -0500

    agrega funcion 'data' y adelanto de funcion saveRegister

---
+++ src/js/data.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+
+window.visitorRegistration = {
+  initializeFirebase: () => {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCsiVpy51uveEfSALI5eonaTBASpjjDwx8',
+      authDomain: 'registro-de-visitas-5f008.firebaseapp.com',
+      databaseURL: 'https://registro-de-visitas-5f008.firebaseio.com',
+      projectId: 'registro-de-visitas-5f008',
+      storageBucket: 'registro-de-visitas-5f008.appspot.com',
+      messagingSenderId: '586868443204'
+    });
+  }
+};
+
