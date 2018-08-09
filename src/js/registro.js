e29750b930b4e477db3f9c8cdebcfe75ed786537
soberanesmajo <soberanesmajo@gmail.com>
2018-08-09 17:20:18 -0500

    agraga registro para perfil de administrador

---
+++ src/js/registro.js
@@ -0,0 +1,23 @@
+visitorRegistration.initializeFirebase();
+
+const mailAdminReg = document.getElementById('mail-register');
+const passAdminReg = document.getElementById('password-register');
+const sendBtn = document.getElementById('send-btn');
+
+sendBtn.addEventListener('click', event => {
+  const adminEmailValue = mailAdminReg.value;
+  const adminPasswordValue = passAdminReg.value;
+  const auth = firebase.auth();
+  
+  const promise = auth.createUserWithEmailAndPassword(adminEmailValue, adminPasswordValue)
+    .then(function(promise) {
+      promise.catch(event => console.log(event.message));
+    });
+});
+  
+firebase.auth().onAuthStateChanged(firebaseUser => { 
+  if (firebaseUser) {
+  } else {
+    console.log('not logged in');
+  }
+});
