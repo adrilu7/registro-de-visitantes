f0aeb4875d20dc6e9e6c4f2abbcfe5598f5c9736
adriana <adrivioleta@hotmail.com>
2018-08-13 17:49:47 -0500

    Comentando funciones

--- src/js/Cam.js
+++ src/js/Cam.js
@@ -1,4 +1,4 @@
-(() => {
+(() => { //  función anónima para evitar las variables globales.
   let streaming = false,
     video = document.querySelector('#video'),
     canvas = document.querySelector('#canvas'),
@@ -6,12 +6,13 @@
     startbutton = document.querySelector('#startbutton'),
     width = 320,
     height = 0;
-
+  // obtener video, compatibilidad de navegadores
   navigator.getMedia = (navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia);
-
+  // Le solicitamos al navegador que nos dé un video sin audio y obtenemos
+  // una secuencia (stream) en la función de retrollamada:
   navigator.getMedia(
     {
       video: true,
@@ -30,7 +31,7 @@
       console.log('An error occured! ' + err);
     }
   );
-
+  // Luego necesitamos configurar el tamaño del video a las dimensiones deseadas.
   video.addEventListener('canplay', (ev) => {
     if (!streaming) {
       height = video.videoHeight / (video.videoWidth / width);
@@ -41,7 +42,10 @@
       streaming = true;
     }
   }, false);
-
+  // En esta función, reestablecemos el tamaño del canvas a las dimensiones del video
+  // el cual lo sustituye y tenemos un marco del video, el cual se copia al canvas.
+  // Luego necesitamos convertir los datos del canvas en datos tipo URL con un encabezado PNG,
+  // y establecer el src de la fotografía a este mismo url.
   takepicture = () => {
     canvas.width = width;
     canvas.height = height;
@@ -49,7 +53,8 @@
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   };
-
+  // capturar una imagen, ahora necesitamos capturar una imagen utilizando (canvas).
+  // Asignamos un manejador de eventos al botón de inicio para llamar a la función de takepicture.
   startbutton.addEventListener('click', (ev) => {
     takepicture();
     ev.preventDefault();
