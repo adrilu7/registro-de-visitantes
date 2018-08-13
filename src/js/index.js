431749bef16a991d888d3f8df5ac613228611cc6
DianaAr8 <dianar_8_8@hotmail.com>
2018-08-13 14:09:53 -0500

    Bajando cambios

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,23 @@
 visitorRegistration.initializeFirebase();
 
 db = firebase.firestore();
-
-// Cerrar sesión
-// Se manda llamar el botón signout para detonar evento
-document.getElementById('signout').addEventListener('click', event => {
-// Para que no se recargue la pagina
-  event.preventDefault();
-  // Se manda llamar la funnción signOut
-  visitorRegistration.signOut();
-});
-
+// Se declara una const para llamar donde se imprimirá la tabla de visitantes
 const table = document.getElementById('table-registers');
+// Se llama la colleccion register por orden descendente de tiempo 
 const registersRef = db.collection('register').orderBy('dateTime', 'desc');
-
+// Se manda traer la colección register
 registersRef.get().then(function(querySnapshot) {
+  // Se itera 
   querySnapshot.forEach(function(doc) {
+    // Se obtienen los datos de los visitantes
     let name = doc.data().name;
     console.log(name);
     let email = doc.data().mail;
     let company = doc.data().company;
     let host = doc.data().host;
     let hour = doc.data().dateTime;
-
+    // Se imprimen los datos obtenidos en una tabla
     table.innerHTML += `<tr>
-   
       <td>${name}</td>
       <td>${email}</td>
       <td>${company}</td>
@@ -33,40 +26,3 @@ registersRef.get().then(function(querySnapshot) {
       </tr>`;
   });
 });
-
-
-// visitorRegistration.initializeFirebase();
-// db = firebase.firestore();
-
-//   Cerrar sesión
-// Se manda llamar el botón signout para detonar evento
-// document.getElementById('signout').addEventListener('click', event => {
-// Para que no se recargue la pagina
-// event.preventDefault();
-// Se manda llamar la funnción signOut
-// visitorRegistration.signOut();
-// });
-
-// const drawVisitors = () => {
-// firebase.auth().onAuthStateChanged(user => { 
-// if (user) { 
-// const registersRef = db.collection('register').orderBy('time', 'desc');
-// registersRef.get()
-// .then(element => {
-// let result = '';
-// element.forEach(register => {
-// result += `<tr>
-   
-// <td>${register.data().name}</td>
-// <td>${register.data().email}</td>
-// <td>${register.data().company}</td>
-// <td>${register.data().host}</td>
-// <td>${register.data().hour}</td>
-// </tr>`;
-// });
-// document.getElementById('table-registers').innerHTML = result;
-// });
-// }
-// });
-// };
-// drawVisitors();
